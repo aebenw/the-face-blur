63ce571fdbec02b501001f856c3d9fa0a1086c7f
github username <github email address>
2020-06-26 18:21:34 -0400

    Bluring image send back

--- fb_server.py
+++ fb_server.py
@@ -1,7 +1,8 @@
-from flask import Flask
-from flask import request
+import io
+from flask import Flask, request, send_file
 from flask_cors import CORS
 import uuid
+from blur_face import blur_image
 
 
 app = Flask(__name__)
@@ -16,14 +17,23 @@ def hello_world():
 def deblob():
     if request.data is not None:
         try:
-            content_header = request.headers['Content-Type']
             # Basing content type off of JS blob type: "image/png"
-            ctype = content_header.rsplit('/', 1)[-1]
+            meme_type = request.headers['X-Content-Type']
+            ctype = meme_type.rsplit('/', 1)[-1]
             rand = uuid.uuid4().hex
-            fh = open(rand + "." + ctype, "wb")
+            file_name = rand + "." + ctype
+            fh = open(file_name, "wb")
             fh.write(request.data)
             fh.close()
-            return "File saved to server"
+            # TODO: IF ctype == .png / jpeg / etc etc - go to image
+            # TODO: IF ctype == .mp4 / etc etc - go to video
+            new_file = blur_image(file_name)
+            with open(new_file, 'rb') as bites:
+                return send_file(
+                    io.BytesIO(bites.read()),
+                    attachment_filename=new_file,
+                    mimetype=meme_type
+                )
 
         except Exception as e:
             app.logger.debug("e", e)
